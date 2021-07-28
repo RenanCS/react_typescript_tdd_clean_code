c5852d13097f6f9960e8be8607f91b90680c0447
Renan Carvalho <renan@interprocess.com.br>
2021-07-28 10:38:02 -0300

    refactor: move httppostclient to its own fille

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,19 +1,9 @@
-import { HttpPostClient } from "domain/data/protocols/http/http-post-client";
+import { HttpPostClientMock } from "../../test/mock-http-client";
 import { RemoteAuthentication } from "./remote-authentication";
 
 
 describe('RemoteAuthentication', () => {
     test('Should call HttpPostClient with correct URL', () => {
-      
-        class HttpPostClientMock implements HttpPostClient {
-            url?: string;
-
-            async post(url: string): Promise<void> {
-                this.url = url;
-                return Promise.resolve();
-            }
-        }
-
         const url = 'any-url';
         const httpPostClientMock = new HttpPostClientMock();
         const sut = new RemoteAuthentication(url, httpPostClientMock);
