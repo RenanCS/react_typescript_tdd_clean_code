ac9af86f2d16527bb11f6dc9ad44de9ef48b7fdf
Renan Carvalho <renan@interprocess.com.br>
2021-07-28 16:41:21 -0300

    feat: ensure axios http client request and body compar

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,16 +1,25 @@
 import { AxiosHttpClient } from "./axios-http-client";
 import axios from 'axios'
 import faker from 'faker'
+import { HttpPostParams } from "@/data/protocols/http";
 
 jest.mock('axios')
 const mockAxios = axios as jest.Mocked<typeof axios>
 
+const makeSurFactory = () : AxiosHttpClient => {
+    return new AxiosHttpClient();
+}
+
+const mockPostRequest = (): HttpPostParams<any> => ({
+    url: faker.internet.url(),
+    body: faker.random.objectElement()
+})
 
 describe('AxiosHttpClient', () => {
-    test('Should call axios with correct url', async () => {
-        const url = faker.internet.url();
-        const sut = new AxiosHttpClient();
-        await sut.post({url: url});
-        expect(mockAxios).toHaveBeenCalledWith(url);
+    test('Should call axios with correct values', async () => {
+        const request = mockPostRequest();
+        const sut = makeSurFactory();
+        await sut.post(request);
+        expect(mockAxios.post).toHaveBeenCalledWith(request.url, request.body);
     })
 })
