c099ae64342067146fc4d7d0dfbaf5d1416521d6
Renan Carvalho <renan@interprocess.com.br>
2021-07-28 10:42:47 -0300

    refactor: move sut creaction to a factory method

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,24 @@
 import { HttpPostClientMock } from "../../test/mock-http-client";
 import { RemoteAuthentication } from "./remote-authentication";
 
+type SutTypes = {
+    sut: RemoteAuthentication
+    httpPostClientMock: HttpPostClientMock
+}
+
+const makeSutFactory = (url: string = 'any-url'): SutTypes => {
+    const httpPostClientMock = new HttpPostClientMock();
+    const sut = new RemoteAuthentication(url, httpPostClientMock);
+    return {
+        sut,
+        httpPostClientMock
+    }
+}
 
 describe('RemoteAuthentication', () => {
     test('Should call HttpPostClient with correct URL', () => {
-        const url = 'any-url';
-        const httpPostClientMock = new HttpPostClientMock();
-        const sut = new RemoteAuthentication(url, httpPostClientMock);
+        const url = 'other_url';
+        const { sut, httpPostClientMock } = makeSutFactory(url);
         sut.auth();
         expect(httpPostClientMock.url).toBe(url);
     })
