87e2a9928e9ae0844b18f110b0244eb9c49b4fdb
Renan Carvalho <renan@interprocess.com.br>
2021-07-29 09:56:13 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,25 @@ module.exports = {
         publicPath: '/public/js',
         filename: 'bundle.js',
     },
+    module: {
+        rules: [{
+            test: /\.ts(x?)$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.scss$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader',
+                options: {
+                    modules: true
+                }
+            }, {
+                loader: 'sass-loader'
+            }]
+        }]
+    },
     resolve: {
         extensions: ['.ts', '.tsx', '.js', 'scss'],
         alias: {
